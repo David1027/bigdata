59f5ba1364447739a817936039c46a5308bb13f1
lijie <lijie@shoestp.cn>
2019-05-22 19:45:37 +0800

    Boot 打包配置

--- main/src/main/java/com/shoestp/mains/config/exceptionHandles/ExceptionHandleConfig.java
+++ main/src/main/java/com/shoestp/mains/config/exceptionHandles/ExceptionHandleConfig.java
@@ -1,27 +1,21 @@
 package com.shoestp.mains.config.exceptionHandles;
 
-import com.shoestp.mains.pojo.MessageResult;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 // @ControllerAdvice(basePackageClasses = AcmeController.class)
-@ControllerAdvice
-public class ExceptionHandleConfig {
-
-  //  @ExceptionHandleConfigler(YourException.class)
-  @ResponseBody
-  MessageResult handleControllerException(HttpServletRequest request, Throwable ex) {
-    HttpStatus status = getStatus(request);
-    return MessageResult.builder().code(1).build();
-  }
-
-  private HttpStatus getStatus(HttpServletRequest request) {
-    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-    if (statusCode == null) {
-      return HttpStatus.INTERNAL_SERVER_ERROR;
-    }
-    return HttpStatus.valueOf(statusCode);
-  }
-}
+//@ControllerAdvice
+//public class ExceptionHandleConfig {
+//
+//  //  @ExceptionHandleConfigler(YourException.class)
+//  @ResponseBody
+//  MessageResult handleControllerException(HttpServletRequest request, Throwable ex) {
+//    HttpStatus status = getStatus(request);
+//    return MessageResult.builder().code(1).build();
+//  }
+//
+//  private HttpStatus getStatus(HttpServletRequest request) {
+//    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+//    if (statusCode == null) {
+//      return HttpStatus.INTERNAL_SERVER_ERROR;
+//    }
+//    return HttpStatus.valueOf(statusCode);
+//  }
+//}
